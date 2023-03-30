2c3582912759bde9238b398de564f968ba57ea6e
Yasmine <yasmine.amaddah@laplateforme.io>
2023-03-30 09:35:39 +0200

    update job3

--- jour3/job03/index.php
+++ jour3/job03/index.php
@@ -14,41 +14,45 @@
         <div id="div0">
         </div>
         <div class="jeu">
-            <div id="div1" data-id="1">
+
+            <div id="div1" data-id="1" data-row="1" data-col="1">
                 <img src="logo1.png" id="logo1" data-id="">
             </div>
-            <div id="div2" data-id="2">
+
+            <div id="div2" data-id="2" data-row="1" data-col="2">
                 <img src="logo2.png" id="logo2" data-id="">
             </div>
-            <div id="div3" data-id="3">
+
+            <div id="div3" data-id="3" data-row="1" data-col="3">
                 <img src="logo3.png" id="logo3" data-id="">
             </div>
-        </div>
 
-        <div class="jeu">
-            <div id="div1" data-id="1">
+            <div id="div4" data-id="1" data-row="2" data-col="1">
                 <img src="logo4.png" id="logo4" data-id="">
             </div>
-            <div id="div2" data-id="2">
+
+            <div id="div5" data-id="2" data-row="2" data-col="2">
                 <img src="logo5.png" id="logo5" data-id="">
             </div>
-            <div id="div3" data-id="3">
+
+            <div id="div6" data-id="3" data-row="2" data-col="3">
                 <img src="logo6.png" id="logo6" data-id="">
             </div>
-        </div>
 
-        <div class="jeu">
-            <div id="div1" data-id="1">
+            <div id="div7" data-id="1" data-row="3" data-col="1">
                 <img src="logo7.png" id="logo7" data-id="">
             </div>
-            <div id="div2" data-id="2">
+
+            <div id="div8" data-id="2" data-row="3" data-col="2">
                 <img src="logo8.png" id="logo8" data-id="">
             </div>
-            <!-- <div id="div3" data-id="3">
-                <img src="logo9.png" id="logo9" data-id="">
-            </div> -->
+
+            <div id="div9" data-id="3" data-row="3" data-col="3">
+                <!-- <img src="logo9.png" id="logo9" data-id=""> -->
+            </div>
         </div>
         
+        <button id="melanger">Melanger</button>
         <button id="recommencer">Recommencer</button>
 
     </main>
@@ -58,10 +62,14 @@
 <style>
     .jeu {
         display: flex;
+        flex-wrap: wrap;
+        width: 600px;
+        height: 600px;
+        margin: auto;
     }
     img {
-        width: 200px;
-        height: 200px;
+        width: 33%;
+        height: 33%;
     }
 </style>
 
--- jour3/job03/script.js
+++ jour3/job03/script.js
@@ -1,9 +1,53 @@
-$("p").empty();
 
 $('#melanger').click(function () {
-   var image = $('#div0').find('img');
+   var image = $('.jeu').find('img');
    image.sort(() => Math.random() - 0.5);
-   $('#div0').append(image);
+   $('.jeu').append(image);
 });
 
-
+$(function() {
+   // Initialisation
+   var emptyRow = 3;
+   var emptyCol = 3;
+   shuffle();
+ 
+   // Click event pour les pièces du puzzle
+   $('.puzzle-piece').click(function() {
+     var row = parseInt($(this).attr('data-row'));
+     var col = parseInt($(this).attr('data-col'));
+ 
+     // Si la pièce est à côté de la case vide, on la déplace
+     if ((row == emptyRow && Math.abs(col - emptyCol) == 1) || (col == emptyCol && Math.abs(row - emptyRow) == 1)) {
+       $(this).animate({
+         top: (emptyRow - 1) * 100 + 'px',
+         left: (emptyCol - 1) * 100 + 'px'
+       }, 200);
+ 
+       // Mise à jour de la position de la case vide
+       $(this).attr('data-row', emptyRow);
+       $(this).attr('data-col', emptyCol);
+       emptyRow = row;
+       emptyCol = col;
+ 
+       // Vérification de la victoire
+       if (isVictory()) {
+         alert('Victoire !');
+       }
+     }
+   });
+ 
+   // Mélange les pièces du puzzle
+   function shuffle() {
+     var pieces = $('.puzzle-piece');
+     for (var i = 0; i < pieces.length; i++) {
+       var randIndex = Math.floor(Math.random() * pieces.length);
+       var tempRow = $(pieces[i]).attr('data-row');
+       var tempCol = $(pieces[i]).attr('data-col');
+       $(pieces[i]).attr('data-row', $(pieces[randIndex]).attr('data-row'));
+       $(pieces[i]).attr('data-col', $(pieces[randIndex]).attr('data-col'));
+       $(pieces[randIndex]).attr('data-row', tempRow);
+       $(pieces[randIndex]).attr('data-col', tempCol);
+     }
+     updatePositions();
+   }
+})
